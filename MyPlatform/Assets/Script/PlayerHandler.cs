5e0c16670950e6fc75791a60f87d72145d0f656d
ragdoll <dambrosio.andry@gmail.com>
2016-04-23 22:46:48 +0200

    aggiunto  PlayerHandler,PlayerMain
    *aggiunti comandi base (salto,movimento)
    *aggiunta GUI provvisoria

--- MyPlatform/Assets/Script/PlayerHandler.cs
+++ MyPlatform/Assets/Script/PlayerHandler.cs
@@ -1,15 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerHandler : MonoBehaviour {
+public class PlayerHandler : MonoBehaviour
+{
+    Rigidbody2D rigidBody2d;
+    Collider2D collider2d;
+
+    public int hp;
+    public int direction;
+    bool canJump;
+    const float speed = 3f;
+    const float jumpForce = 350f;
 
 	// Use this for initialization
-	void Start () {
-	
+    void Start () 
+    {
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+    public void HandlerUpdate () 
+    {
+        Movement();
 	}
+
+    public void PlayerStart ()
+    {
+        rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
+        collider2d = gameObject.GetComponent<Collider2D>();
+    }
+
+    public void SetDirection(int dir)
+    {
+        if (canJump)
+        {
+            direction = dir;
+        }
+    }
+
+    public void TryJump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            rigidBody2d.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    void Movement()
+    {
+        
+        //movmento destra e sinistra
+        if(Input.GetKey(KeyCode.D) && canJump)
+        {
+            direction = 1;
+        }
+        if(Input.GetKey(KeyCode.A) && canJump)
+        {
+            direction = -1;
+        }
+        if (direction != 0)
+        {
+            if (direction == 1)
+            {
+                rigidBody2d.velocity = Vector2.right *speed;
+            }
+            if (direction == -1)
+            {
+                rigidBody2d.velocity = Vector2.left *speed;
+            }
+        }
+        //jump
+        if (Input.GetKey(KeyCode.W) && canJump)
+        {
+            canJump = false;
+            rigidBody2d.AddForce(Vector3.up * jumpForce);
+        }
+        //
+        direction = 0;
+    }
+
+    void OnCollisionEnter2D()
+    {
+        canJump = true;
+    }
 }
---
+++ MyPlatform/Assets/Script/PlayerMain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMain : PlayerHandler
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerStart();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        HandlerUpdate();
+    }
+}
